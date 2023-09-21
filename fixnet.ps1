b1946a8419a1baa0a23dfbac6e0f22f9156d8242
fthr1 <79537977+fthr1@users.noreply.github.com>
2023-09-21 06:59:21 +0100

    Update fixnet.ps1
    fix hetzner IP windows server

--- fixnet.ps1
+++ fixnet.ps1
@@ -3,6 +3,7 @@
 # optional parameter:  -ExecutionPolicy Bypass -File C:\fixnet.ps1
 
 # CIDR example: 32, 24, 26, 27
+# See this table: https://1.bp.blogspot.com/-LBtrUz0DwTI/WJbGPxkrIOI/AAAAAAAAAfc/q12iYvn9AYwQ-Fo_lCuI4EsqC3EXs2m_ACEw/s1600/ip2.png
 foreach($Adapter in Get-NetAdapter)
 {
 New-NetIPAddress –IPAddress YOUR_WINDOWS_SERVER_IP -DefaultGateway YOUR_GATEWAY_IP -PrefixLength CHANGE_THIS_WITH_CIDR -InterfaceIndex $Adapter.InterfaceIndex
